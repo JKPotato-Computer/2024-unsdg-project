1e4a83666a1d4187a35ba76a8da5993481adb8e3
JKPotato-Computer <jkpotato.compurer@gmail.com>
2024-04-22 10:20:54 -0500

    Update game.js to generate random buildings... wheneever the implementation exists.

--- js/game.js
+++ js/game.js
@@ -1,8 +1,31 @@
 const game = (function() {
 	let gameData = {
 		districtPropertyData : [],
-		streetPropertyData : []
+		streetPropertyData : [],
+		budget : 0,
+		yearlyDue : 0,
+		debt : 0,
+		SDG : {},
+		mapSize : "city"
 	};
+
+	const generateRanndomProperties = function() {
+		switch (gameData.mapSize) {
+			case "village":
+				break;
+			case "town": 
+				break;
+			case "city":
+				break;
+		}
+
+		/*
+		1) Create a new class Building with parameters of attempted spawnpoint (x,y) from bottom left of building
+		2) Check to see if there's space to place the building at that location. If not, regenerate until avaliable.
+		3) IF there is space, place the building into that grid location.
+		4) Pathfind a road from that item to a nearby road, business, or state route.
+		*/
+	}
 	
 	const createPropertyGrid = function(size) {
 		gameData.districtPropertyData = new Array(size);
